26713bb30528ae95e86e1d9101180c7907abe956
mikita kavaleuski <nikcrafter1999@gmail.com>
2021-06-24 10:19:49 +0300

    Added FETCH API functions

--- js/script.js
+++ js/script.js
@@ -258,27 +258,32 @@ window.addEventListener("DOMContentLoaded", function () {
         margin: 0 auto;
       `;
       form.insertAdjacentElement("afterend", statusMessage);
-      const request = new XMLHttpRequest();
-      request.open("POST", "server.php");
       // request.setRequestHeader("Content-type", "multipart/form-data"); // При использовании FormData заголовок не нужен.
       // request.setRequestHeader("Content-type", "application/json"); // Здесь и далее - при реализации через JSON.
       const formData = new FormData(form);
-      // const object = {};
-      // formData.forEach(function (value, key) {
-      //  object[key] = value;
-      // });
-      // request.send(JSON.stringify(object));
-      request.send(formData);
-      request.addEventListener("load", () => {
-        if (request.status === 200) {
-          console.log(request.response);
+      const object = {};
+      formData.forEach(function (value, key) {
+        object[key] = value;
+      });
+      fetch("server.php", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(object),
+      })
+        .then((data) => data.text())
+        .then((data) => {
+          console.log(data);
           showThanksModal(message.success);
+        })
+        .catch(() => {
+          showThanksModal(message.failure);
+        })
+        .finally(() => {
           form.reset();
           statusMessage.remove();
-        } else {
-          showThanksModal(message.failure);
-        }
-      });
+        });
     });
   }
 
